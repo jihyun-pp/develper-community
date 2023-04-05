5fc7212a5a702702ef14365ce42e84596193254f
jihyun-pp <qkrwlw1211@gmail.com>
2023-04-05 23:10:20 +0900

    기본 스키마 추가

--- backend/app/schema/schemas.py
+++ backend/app/schema/schemas.py
@@ -12,3 +12,38 @@ class User(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Category(BaseModel):
+    cid: int
+    category_nm: str
+
+    class Config:
+        orm_mode = True
+
+
+class Board(BaseModel):
+    bid: int
+    category: str
+    uid: int  # user
+    title: str
+    content: str
+    createdAt: datetime
+    updatedAt: datetime
+    hit: int
+    imgPath: str
+
+    class Config:
+        orm_mode = True
+
+
+class Reply(BaseModel):
+    rid: int
+    bid: int
+    uid: int
+    reply: str
+    createdAt: datetime
+    updatedAt: datetime
+
+    class Config:
+        orm_mode = True

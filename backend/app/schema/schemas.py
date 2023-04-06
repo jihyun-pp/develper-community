9b974afa0aee3e5386159b9fb07b71c5abb6288f
jihyun-pp <qkrwlw1211@gmail.com>
2023-04-06 16:17:57 +0900

    join, login user schema

--- backend/app/schema/schemas.py
+++ backend/app/schema/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, EmailStr
 from datetime import datetime
 
 class User(BaseModel):
@@ -7,11 +7,37 @@ class User(BaseModel):
     password: str
     username: str
     email: str
-    auth: str
+    auth: str | None = None  # nullable
     createdAt: datetime
+    updatedAt: datetime
 
     class Config:
         orm_mode = True
+
+class CreateUser(BaseModel):
+    user_id: str
+    password1: str
+    password2: str
+    username: str
+    email: EmailStr
+
+    @validator('user_id', 'password1', 'username', 'email')
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError('빈 값은 허용되지 않습니다.')
+        return v
+
+    @validator('password2')
+    def password_match(cls, v, values):
+        if 'password1' in values and v != values['password1']:
+            raise ValueError('비밀번호가 일치하지 않습니다.')
+        return v
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    user_id: str
 
 
 class Category(BaseModel):

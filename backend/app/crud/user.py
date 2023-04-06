465b8e6942d562618e2c8445849da0e701e40e81
jihyun-pp <qkrwlw1211@gmail.com>
2023-04-06 16:59:43 +0900

    join, login 추가

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,10 +1,35 @@
-# from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from backend.app.model import User
+from app.model import User
 from sqlalchemy import select, text
+import app.schema.schemas as schema
+from passlib.context import CryptContext
 
-def get_user_list(db: Session):
-    result = db.execute(text("select * from tb_user"))
-    # result = db.execute(select(User))
-    # print(result.all())
-    return result.all()
+
+def user_list(db: Session):
+    result = db.query(User).order_by(User.uid.desc()).all()
+    return result
+
+def create_user(db: Session, _create_user: schema.CreateUser):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    c_user = User(
+        user_id=_create_user.user_id,
+        username=_create_user.username,
+        email=_create_user.email,
+        password=pwd_context.hash(_create_user.password1),
+    )
+    db.add(c_user)
+    db.commit()
+
+def get_existing_user(db: Session, _create_user: schema.CreateUser):
+    return db.query(User).filter(User.user_id == _create_user.user_id).first()
+
+def get_user(db: Session, user_id: str):
+    return db.query(User).filter(User.user_id == user_id).first()
+
+
+def modify_user(db: Session, user_id: str, username: str):
+    a = db.query(User).filter(User.user_id == user_id).all()
+    a.username = username
+    db.commit()
+    return a.username
+

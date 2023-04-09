63d6f804f151a854b21974b32c96f62b7376ef41
jihyun-pp <qkrwlw1211@gmail.com>
2023-04-09 15:52:20 +0900

    board crud 추가

--- backend/app/crud/board.py
+++ backend/app/crud/board.py
@@ -1,8 +1,55 @@
 from sqlalchemy.orm import Session
-from app.model import Board
+from app.model import Board, User
 from sqlalchemy import select, text
 import app.schema.schemas as schema
 from passlib.context import CryptContext
+from datetime import datetime
 
-def board_list(db: Session):
+def get_contents_list(db: Session):
     return db.query(Board).order_by(Board.bid.desc()).all()
+
+def get_user_contents(db: Session, uid: int):
+    return db.query(Board).filter(Board.uid==uid).order_by(Board.bid.desc()).all()
+
+def get_content(db: Session, bid: int):
+    return db.query(Board).filter(Board.bid==bid).first()
+
+def create_board(db: Session, _board=schema.CreateBoard):
+    try:
+        board = Board(
+            category=_board.category,
+            uid=1,
+            title=_board.title,
+            content=_board.content,
+            imgPath=_board.imgPath
+        )
+        db.add(board)
+        db.commit()
+        return {"result": "S"}
+    except Exception as e:
+        return {"result": f"F : {e}"}
+
+
+def update_board(db: Session, bid: int, _board=schema.UpdateBoard):
+    try:
+        board = db.query(Board).filter(Board.bid == bid).first()
+        board.title = _board.title
+        board.content = _board.content
+        board.imgPath = _board.imgPath
+        board.updatedAt = datetime.now()
+        db.commit()
+
+        return {"result": "S"}
+    except Exception as e:
+        return {"result": f"F : {e}"}
+
+
+def delete_board(db: Session, bid: int):
+    try:
+        # db.delete(Board).where(Board.bid == bid)
+        content = db.query(Board).filter(Board.bid == bid).first()
+        db.delete(content)
+        db.commit()
+        return {"result": "S"}
+    except Exception as e:
+        return {"result": f"F : {e}"}
--- backend/app/router/board.py
+++ backend/app/router/board.py
@@ -5,14 +5,62 @@ from datetime import datetime, timedelta
 
 from app.database import get_db
 import app.schema.schemas as schema
-from app.model import Board
+from app.model import Board, User
 import app.crud.board as crud
+from app.router.user import get_current_user
 
 router = APIRouter(
     prefix='/api/board',
 )
 
-@router.get('/list')
-def board_list(db: Session = Depends(get_db)):
-    result = crud.board_list(db=db)
+@router.get('/list', response_model=list[schema.Board])
+def all_contents(db: Session = Depends(get_db)):
+    result = crud.get_contents_list(db=db)
     return result
+
+@router.get("/user/{uid}")
+def user_contents(uid: int, db: Session = Depends(get_db)):
+    result = crud.get_user_contents(db=db, uid=uid)
+    return result
+
+@router.get("/{bid}")
+def get_content(bid: int, db: Session = Depends(get_db)):
+    result = crud.get_content(db=db, bid=bid)
+    return result
+
+# @router.post('/create', response_model=schema.Board)
+# def create_board(board: schema.CreateBoard,
+#                  db: Session = Depends(get_db),
+#                  user: User = Depends(get_current_user)):
+#     try:
+#         crud.create_board(db=db, _board=board, user=user.uid)
+#     except Exception as e:
+#         print(e)
+
+@router.post('/create-nonuser', response_model=schema.Board)
+def create_board(board: schema.CreateBoard, db: Session = Depends(get_db)):
+    try:
+        result = crud.create_board(db=db, _board=board)
+    except Exception as e:
+        return {"error": e}
+
+    return result
+
+
+@router.put("/modify")
+def update_board(bid: int, board: schema.UpdateBoard, db: Session = Depends(get_db)):
+    try:
+        result = crud.update_board(db=db, bid=bid, _board=board)
+    except Exception as e:
+        result = {"error": e}
+    return result
+
+
+@router.post("/delete")
+def delete_board(bid: int, db: Session = Depends(get_db)):
+    try:
+        result = crud.delete_board(db=db, bid=bid)
+    except Exception as e:
+        result = {"result": e}
+    return result
+

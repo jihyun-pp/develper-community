0a4b87a5609a5d2e2aaa419f516c039700a4f2a4
jihyun-pp <qkrwlw1211@gmail.com>
2023-04-12 16:41:38 +0900

    router 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,14 +3,16 @@ from fastapi import FastAPI
 
 import app.router.user as user_api
 import app.router.board as board_api
+import app.router.reply as reply_api
 
 def create_app():
     app = FastAPI()
     app.include_router(user_api.router)
     app.include_router(board_api.router)
+    app.include_router(reply_api.router)
     return app
 
 app = create_app()
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

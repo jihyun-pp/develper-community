ff30ca40e70c1fa6c519d0cd7d42a81785f2c541
jihyun-pp <qkrwlw1211@gmail.com>
2023-05-13 23:47:42 +0900

    resolve #3

--- app/database.py
+++ app/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.core.env import environ
 
+
 engine = create_engine(environ('DB_CONN_URL'), echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()

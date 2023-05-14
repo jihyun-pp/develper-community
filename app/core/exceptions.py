0772bb03dba87daa1eb4fb23f6106fa7cb6ddaf0
jihyun-pp <qkrwlw1211@gmail.com>
2023-05-14 21:23:54 +0900

    exceptions

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -13,22 +13,27 @@ class BadRequestException(CustomException):
     code = HTTPStatus.BAD_REQUEST
     error_code = HTTPStatus.BAD_REQUEST
     message = HTTPStatus.BAD_REQUEST.description
+
 class NotFoundException(CustomException):
     code = HTTPStatus.NOT_FOUND
     error_code = HTTPStatus.NOT_FOUND
     message = HTTPStatus.NOT_FOUND.description
+
 class ForbiddenException(CustomException):
     code = HTTPStatus.FORBIDDEN
     error_code = HTTPStatus.FORBIDDEN
     message = HTTPStatus.FORBIDDEN.description
+
 class UnauthorizedException(CustomException):
     code = HTTPStatus.UNAUTHORIZED
     error_code = HTTPStatus.UNAUTHORIZED
     message = HTTPStatus.UNAUTHORIZED.description
+
 class UnprocessableEntity(CustomException):
     code = HTTPStatus.UNPROCESSABLE_ENTITY
     error_code = HTTPStatus.UNPROCESSABLE_ENTITY
     message = HTTPStatus.UNPROCESSABLE_ENTITY.description
+
 class DuplicateValueException(CustomException):
     code = HTTPStatus.UNPROCESSABLE_ENTITY
     error_code = HTTPStatus.UNPROCESSABLE_ENTITY
@@ -38,6 +43,7 @@ class DecodeTokenException(CustomException):
     code = 400
     error_code = 10000
     message = "token decode error"
+
 class ExpiredTokenException(CustomException):
     code = 400
     error_code = 10001

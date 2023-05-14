9f79358fb9eccef520a876d6cfb636890340c58e
jihyun-pp <qkrwlw1211@gmail.com>
2023-05-14 21:30:13 +0900

    테스트위한 기본 라우터 추가

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import app.router.users as users
+
 def create_app():
     app = FastAPI()
+    app.include_router(users.router)
     return app
 
 app = create_app()
@@ -16,6 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=[]
 )
+
+@app.get("/")
+async def root():
+    return {"msg": "Test! Hello!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)

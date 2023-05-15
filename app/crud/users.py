f3a16294e2f0fe4a60ea3fc7307c4f79b4fe015f
jihyunpark <jihyunpark@goldenplanet.co.kr>
2023-05-15 22:50:41 +0900

    [feat] login, create 기능 추가

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,29 +1,63 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from starlette import status
 from passlib.context import CryptContext
+from datetime import datetime
 
 from app.model import User
 import app.schema.schemas as schema
+from app.core.exceptions import NotFoundException, DuplicateEmailException, BadRequestException
+from app.core.token import Token
+from app.schema.users.request import *
+from app.schema.users.response import *
 
-def create_user(db: Session, _create_user: schema.NewUser):
-    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    c_user = User(
-        user_id=_create_user.user_id,
-        username=_create_user.username,
-        email=_create_user.email,
-        password=pwd_context.hash(_create_user.password1),
-    )
-    db.add(c_user)
+
+def modify_user(db: Session, email: str, nickname: str):
+    a = db.query(User).filter(User.email == email).all()
+    a.nickname = nickname
     db.commit()
+    return a.nickname
 
-def get_existing_user(db: Session, _create_user: schema.NewUser):
-    return db.query(User).filter(User.user_id == _create_user.user_id).first()
+class UserCrud:
+    async def login(self, db: Session, email: str, password: str):
+        try:
+            pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+            user = db.query(User).filter(User.email == email).first()
+            if not user:
+                raise NotFoundException
+            elif not pwd_context.verify(password, user.password):
+                raise NotFoundException
 
-def get_user(db: Session, user_id: str):
-    return db.query(User).filter(User.user_id == user_id).first()
+            return LoginResponseSchema(
+                token=Token.encode(payload={'user_id': user.email}),
+                refresh_token=Token.encode(payload={"sub": "refresh"})
+            )
 
+        except Exception as e:
+            return str(e)
 
-def modify_user(db: Session, user_id: str, username: str):
-    a = db.query(User).filter(User.user_id == user_id).all()
-    a.username = username
-    db.commit()
-    return a.username
+    async def create_user(self, db: Session, req: RequestCreateUser):
+        result = {}
+        try:
+            is_exist = db.query(User).filter(User.email == req.email).first()
+            if is_exist:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 사용자입니다.")
+
+            pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+            user = User(
+                email=req.email,
+                password=pwd_context.hash(req.password1),
+                nickname=req.nickname,
+                # birth=req.birth,
+                auth='p011',
+                createdAt=datetime.now(),
+                updatedAt=datetime.now()
+            )
+            db.add(user)
+            db.commit()
+            result['message'] = 'OK'
+
+        except Exception as e:
+            result['message'] = str(e)
+
+        return result
